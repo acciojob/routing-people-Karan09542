dff4d1f4ed51582f536c840e51ccda539c452c5c
Karan09542 <karan09542@gmail.com>
2025-11-16 16:22:35 +0530

    router example

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,18 @@
 
 import React from "react";
 import './../styles/App.css';
+import UserList from "./USerList";
+import { Route, Routes } from "react-router-dom";
+import UserDetails from "./UserDetails";
 
 const App = () => {
   return (
     <div>
         {/* Do not remove the main div */}
+        <Routes>
+          <Route path="/" element={<UserList/>}/>
+          <Route path="/users/:id" element={<UserDetails/>}/>
+        </Routes>
     </div>
   )
 }
---
+++ src/components/UserDetails.js
@@ -0,0 +1,27 @@
+import React, { useEffect, useState } from 'react'
+import { useParams } from 'react-router-dom'
+
+const UserDetails = () => {
+    const {id} = useParams();
+    const [user, setUser] = useState(null);
+    useEffect(() => {
+          fetch(`https://jsonplaceholder.typicode.com/users/${id}`)
+          .then(res => res.json())
+          .then(data => {
+            setUser(data)
+          })
+      },[id,user])
+    if(!user) return <p>Loading...</p>;
+  return (
+    <div>
+        <h1>User Details</h1>
+        <p>Name: {user?.name}</p>
+        <p>Username: {user?.username}</p>
+        <p>Email: {user?.email}</p>
+        <p>Phone: {user?.phone}</p>
+        <p>Website: {user?.website}</p>
+    </div>
+  )
+}
+
+export default UserDetails
